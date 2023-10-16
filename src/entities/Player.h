faff974f6c71ac7a64159db5239f8bf9fd49e9cc
Bugado <addminplayerbr1122@gmail.com>
2023-10-16 14:22:17 -0300

    chore: formatação do código

--- src/entities/Player.h
+++ src/entities/Player.h
@@ -52,22 +52,26 @@ namespace Player {
         switch(pressedKey){
             case 72: case 'w': /** cima */
                 if(canMove(row - 1 , column)){
-                    row--;}
+                    row--;
+                }
                 break;
 
             case 80: case 's': /** baixo */
                 if(canMove(row + 1 , column)){
-                    row++;}
+                    row++;
+                }
                 break;
 
             case 75: case 'a': /** esquerda */
                 if(canMove(row, column - 1)){
-                    column--;}
+                    column--;
+
                 break;
 
             case 77: case 'd': /** direita */
                 if(canMove(row, column + 1)){
-                    column++;}
+                    column++;
+                }
                 break;
 
             case 32: case 13: /** dropar bomba */
