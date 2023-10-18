6b74f552bbb823be5c9d55b36e6918ec466fc461
Jordan <lippertx11@gmail.com>
2023-10-17 21:10:51 -0300

    feat: Consumables

---
+++ src/entities/Consumables.h
@@ -0,0 +1,13 @@
+//
+// Created by Jordan on 17/10/2023.
+//
+
+#ifndef GAME02_CBP_CONSUMABLES_H
+#define GAME02_CBP_CONSUMABLES_H
+
+struct Consumables{
+
+};
+
+
+#endif //GAME02_CBP_CONSUMABLES_H
